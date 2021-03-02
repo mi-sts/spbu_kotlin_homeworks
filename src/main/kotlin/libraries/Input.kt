46960437061aa588ff5f09d4f32090b25169c6e2
mi-sts <66365072+mi-sts@users.noreply.github.com>
2021-03-02 20:05:52 +0300

    Update solution

--- src/main/kotlin/libraries/Input.kt
+++ src/main/kotlin/libraries/Input.kt
@@ -5,7 +5,7 @@ object Input {
         print(preInputMessage)
 
         var number = readLine()?.toLongOrNull() ?: 0L
-        while (number <= 0) {
+        while (number <= 0L) {
             println("Incorrect input! Enter a natural number:")
             number = readLine()?.toLongOrNull() ?: 0L
         }
@@ -17,7 +17,7 @@ object Input {
         print(preInputMessage)
 
         var number = readLine()?.toLongOrNull() ?: -1L
-        while (number < 0) {
+        while (number < 0L) {
             println("Incorrect input! Enter a non-negative number:")
             number = readLine()?.toLongOrNull() ?: 0L
         }
