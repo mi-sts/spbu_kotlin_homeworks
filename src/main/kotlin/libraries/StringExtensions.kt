44bb71965c020c838df8c7571da4f068d2ca3451
mi-sts <66365072+mi-sts@users.noreply.github.com>
2021-03-04 19:29:15 +0300

    Fix multi line

--- src/main/kotlin/libraries/StringExtensions.kt
+++ src/main/kotlin/libraries/StringExtensions.kt
@@ -1,8 +1,7 @@
 package libraries
 
 fun String.findSubstringCount(subString: String): Int {
-    if (subString.length == 0)
-        return 0
+    if (subString.length == 0) return 0
 
     return this.windowed(subString.length).count { it == subString }
 }
