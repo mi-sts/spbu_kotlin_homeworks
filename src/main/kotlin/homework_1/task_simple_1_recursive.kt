6afcb6e32ee25b4f31e218ff53fc4fc9bfa3d122
mi-sts <66365072+mi-sts@users.noreply.github.com>
2021-03-13 21:48:56 +0300

    Fix the documentation format

--- src/main/kotlin/homework_1/task_simple_1_recursive.kt
+++ src/main/kotlin/homework_1/task_simple_1_recursive.kt
@@ -2,11 +2,11 @@ package homework_1
 
 import libraries.Input
 
-/*
-Calculates the factorial recursively.
-@param[number] the value used to calculate the factorial.
-@return the factorial of the number.
-*/
+/**
+ * Calculates the factorial recursively.
+ * @param[number] the value used to calculate the factorial.
+ * @return the factorial of the number.
+ **/
 private fun findFactorialRecursive(number: Long): Long {
     return if (number in 0..1) 1 else number * findFactorialRecursive(number - 1)
 }
