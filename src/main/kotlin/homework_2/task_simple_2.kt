1cf7828628250057267f0213477a8d15f6c3e746
mi-sts <66365072+mi-sts@users.noreply.github.com>
2021-03-14 12:31:45 +0300

    Add soltuion for task_simple_2 and change array extension function

--- src/main/kotlin/homework_2/task_simple_2.kt
+++ src/main/kotlin/homework_2/task_simple_2.kt
@@ -1,2 +1,21 @@
 package homework_2
 
+import libraries.Input
+import libraries.deleteDuplicates
+
+/**
+ * Prints the array without duplicates.
+ * @param[arrayWithoutDuplicates] the processed array.
+ */
+private fun printResult(arrayWithoutDuplicates: Array<Long>) {
+    println("The number array without duplicates:")
+    println(arrayWithoutDuplicates.contentToString())
+}
+
+fun main() {
+    val preInputMessage = "Enter the numbers separated be a space."
+    val numbersArray: Array<Long> = Input.getNumbersList(preInputMessage).toTypedArray()
+    val numberArrayWithoutDuplicated: Array<Long> = numbersArray.deleteDuplicates()
+
+    printResult(numberArrayWithoutDuplicated)
+}
--- src/main/kotlin/libraries/ArrayExtensions.kt
+++ src/main/kotlin/libraries/ArrayExtensions.kt
@@ -4,4 +4,5 @@ package libraries
  * Array extension to delete duplicates elements.
  * Only the right occurrences remain. They keep the original order.
  */
-fun <T> Array<T>.deleteDuplicates() = this.apply { reverse() }.distinct().apply { reverse() }
+inline fun <reified T> Array<T>.deleteDuplicates(): Array<T> = this.reversed().distinct().reversed().toTypedArray()
+
