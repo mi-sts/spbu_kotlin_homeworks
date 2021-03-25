8c6fdbd1fa025972c1b15f79d1b9f6090fd3c291
mi-sts <66365072+mi-sts@users.noreply.github.com>
2021-03-25 21:39:56 +0300

    Update solution

--- src/main/kotlin/homework_2/task_simple_2.kt
+++ src/main/kotlin/homework_2/task_simple_2.kt
@@ -9,9 +9,9 @@ private fun printResult(arrayWithoutDuplicates: LongArray) {
 }
 
 fun main() {
-    val preInputMessage = "Enter the numbers separated be a space:\n"
-    val numbersArray = Input.getNumbersList(preInputMessage).toTypedArray()
+    val preInputMessage = "Enter the numbers separated by a space:\n"
+    val numbersArray = Input.getNumbersList(preInputMessage).toLongArray()
     val numberArrayWithoutDuplicated = numbersArray.deleteDuplicates()
 
-    printResult(numberArrayWithoutDuplicated.toLongArray())
+    printResult(numberArrayWithoutDuplicated)
 }
--- src/main/kotlin/libraries/ArrayExtensions.kt
+++ src/main/kotlin/libraries/ArrayExtensions.kt
@@ -4,4 +4,4 @@ package libraries
  * Array extension to delete duplicates elements.
  * Only the right occurrences remain. They keep the original order.
  */
-inline fun <reified T> Array<T>.deleteDuplicates(): Array<T> = this.reversed().distinct().reversed().toTypedArray()
+fun LongArray.deleteDuplicates(): LongArray = this.reversed().distinct().reversed().toLongArray()
