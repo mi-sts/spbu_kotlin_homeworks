7a7131f79f35c8e69a0e34eaf4888235f67b6796
mi-sts <66365072+mi-sts@users.noreply.github.com>
2021-03-26 12:49:29 +0300

    Resolve conflicts

--- src/main/kotlin/libraries/PriorityQueue.kt
+++ src/main/kotlin/libraries/PriorityQueue.kt
@@ -2,7 +2,7 @@ package libraries
 
 import java.util.PriorityQueue
 
-class PriorityQueue<E : Any>() {
+class PriorityQueue<E : Any> {
     class PriorityElement<E : Any>(private val element: E, private val priority: Double) :
         Comparable<PriorityElement<E>> {
         override operator fun compareTo(other: PriorityElement<E>): Int {
@@ -14,18 +14,18 @@ class PriorityQueue<E : Any>() {
         fun getElement(): E = this.element
     }
 
-    private val priorityQueue: PriorityQueue<PriorityElement<E>> = PriorityQueue<PriorityElement<E>>()
+    private val queue: PriorityQueue<PriorityElement<E>> = PriorityQueue<PriorityElement<E>>()
 
-    fun enqueue(element: E, priority: Double) = priorityQueue.add(PriorityElement(element, priority))
+    fun enqueue(element: E, priority: Double) = queue.add(PriorityElement(element, priority))
 
     fun peek(): E {
-        if (priorityQueue.isEmpty()) throw Exception("Priority queue is empty!")
-        return priorityQueue.peek().getElement()
+        if (queue.isEmpty()) throw ArrayIndexOutOfBoundsException()
+        return queue.peek().getElement()
     }
 
     fun remove() {
-        if (priorityQueue.isEmpty()) throw Exception("Priority queue is empty!")
-        priorityQueue.remove(priorityQueue.peek())
+        if (queue.isEmpty()) throw ArrayIndexOutOfBoundsException()
+        queue.remove(queue.peek())
     }
 
     fun roll(): E {
