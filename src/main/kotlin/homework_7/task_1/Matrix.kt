dedce64a09f1ef3cb57a663ad92fcdb5ec7996ba
mi-sts <66365072+mi-sts@users.noreply.github.com>
2021-05-28 10:36:59 +0300

    Fix require

--- src/main/kotlin/homework_7/task_1/Matrix.kt
+++ src/main/kotlin/homework_7/task_1/Matrix.kt
@@ -5,21 +5,20 @@ import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.joinAll
-import java.lang.IllegalArgumentException
 
 data class Matrix(private val matrixArray: Array<DoubleArray>) {
     private val height = matrixArray.size
     private val width = matrixArray[0].size
 
     init {
-        require(matrixArray.isNotEmpty() && matrixArray[0].isNotEmpty()) {
-            IllegalArgumentException("The matrix size cannot be zero!") }
+        require(matrixArray.isNotEmpty() && matrixArray[0].isNotEmpty()) { "The matrix size cannot be zero!" }
+        require(matrixArray.all { it.count() == matrixArray[0].count() }) {
+            "The matrix row sizes cannot have different lengths"
+        }
     }
 
     suspend operator fun times(other: Matrix): Matrix {
-        require(width == other.height) {
-            IllegalArgumentException("Incorrect matrix sizes for the multiplication!")
-        }
+        require(width == other.height) { "Incorrect matrix sizes for the multiplication!" }
 
         val resultMatrixArray: Array<DoubleArray> = Array(height) { DoubleArray(other.width) { 0.0 } }
         val requests = ArrayList<Job>()
